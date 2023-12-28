a183e71d5ed9ad07411e9a831a992969bc9cd579
jasonpogi <51401355+mgalang229@users.noreply.github.com>
2023-12-28 21:25:34 +0800

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string finalString(string s) {
+        string ans = "";
+        for (char& c : s) {
+            if (c != 'i') {
+                ans += c;
+            } else {
+                reverse(ans.begin(), ans.end());
+            }
+        }
+        return ans;
+    }
+};
